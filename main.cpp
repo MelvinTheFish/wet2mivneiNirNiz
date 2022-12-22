a14239ff1a9ce164ed51f55448ae46aef68465b3
MelvinTheFish <nizniz.kr@gmail.com>
2022-12-22 15:02:45 +0200

    test not working ashdlfjashkdfjasd
    but i need to go

--- main.cpp
+++ main.cpp
@@ -3,54 +3,48 @@
 #include "Team.h"
 #include "Player.h"
 #include <memory>
+#include <stdlib.h>     /* srand, rand */
+#include "Player_UnionFind_Node.h"
+
 
 using namespace std;
 
-
+void fill_random_permutation (int * a){
+    int used[5] = {0, 0, 0, 0, 0};
+    for (int i = 0; i < 5; ++i) {
+        int j = rand()%5;
+        while (used[j]){
+            j = rand()%5;
+        }
+        used[j] = 1;
+        a[i] = j;
+    }
+}
 
 int main() {
 
-    weak_ptr<Player> kal = weak_ptr<Player>();
-    shared_ptr<Player> realKal = kal.lock();
-    if(realKal){
-        cout << "not null" << endl;
-    }
-    else{
-        cout << "null" << endl;///good
-    }
+    srand((unsigned int)time(NULL));
+    shared_ptr<Team> teams[5];
+    shared_ptr<Player> players[25];
 
+    int id = 1;
 
-    shared_ptr<Player> player1 = make_shared<Player>(1);
+    int arr[5] = {0,0,0,0,0};
 
-    kal = player1;
-
-
-    realKal = kal.lock();
-    if(realKal){
-        cout << "not null" << endl;///good
-    }
-    else{
-        cout << "null" << endl;
+    for (int i = 0; i < 5; ++i) {
+        teams[i] = make_shared<Team>(i);
+        for (int j = 0; j < 5; ++j) {
+            if(id-1 < 24){
+                fill_random_permutation(arr);
+                players[id-1] = make_shared<Player>(id, i, permutation_t(arr), 0, 0, 0, true);
+                Player_UnionFind_Node node = Player_UnionFind_Node(players[id-1], teams[i]);
+                id++;
+            }
+        }
     }
 
-    kal.reset();
 
-    realKal = kal.lock();
-    if(realKal){
-        cout << "not null" << endl;
-    }
-    else{
-        cout << "null" << endl;///good
-    }
 
-    kal = player1;
-    realKal = kal.lock();
-    if(realKal){
-        cout << "not null" << endl;///good
-    }
-    else{
-        cout << "null" << endl;
-    }
 
     return 0;
 }
